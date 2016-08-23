40de8acb4141df703a30c770cbb7646d5a388e56
Marcio Domingues <fc42555@alunos.fc.ul.pt>
2016-08-24 00:55:13 +0100

    flask news feed using flask routes for differente origins of feeds

--- Headlines/headlines.py
+++ Headlines/headlines.py
@@ -4,25 +4,39 @@ from flask import Flask
 
 app = Flask( __name__ )
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
 
 
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+
+
+@app.route("/bbc")
+def bbc():
+    return get_news('bbc')
+
+
+@app.route("/cnn")
+def cnn():
+    return get_news('cnn')
+
+
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    #print first_article
     return """<html>
-        <body>
-            <h1> BBC Headline </h1>
-            <b>{0}</b> <br/>
-            <i>{1}</i> <br/>
-            <p>{2}</p> <br/>
-        </body>
-    </html>""".format(first_article.get("title"), first_article.
-   get("published"), first_article.get("summary"))
-    #return "shit"
+       <body>
+           <h1>Headlines </h1>
+           <b>{0}</b> </ br>
+           <i>{1}</i> </ br>
+           <p>{2}</p> </ br>
+       </body>
+   </html>""".format(first_article.get("title"), first_article.
+                     get("published"), first_article.get("summary"))
 
-if __name__ == '__main__':
-    app.run( port=5000, debug=True )
 
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
